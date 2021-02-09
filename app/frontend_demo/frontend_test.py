0ebbf3cf0c2e1e01c895a31c30da9e25299bafd4
kay的小屋 <47267728+kay-cottage@users.noreply.github.com>
2021-02-09 12:10:21 +0800

    Update and rename app/.idea/xx.xml to app/frontend_demo/frontend_test.py

---
+++ app/frontend_demo/frontend_test.py
@@ -0,0 +1,15 @@
+  
+import requests
+import base64
+import json
+ 
+url = input('请输入API的URL:')
+#url = 'http://192.18.31.31:5000/'
+
+try:
+    files = {"pic":open("2.jpg","rb")}
+    response = requests.post(url,files = files)
+    print('上传成功')
+    print(json.loads(response.text))
+except:
+    print('上传失败，请重新检查操作')
